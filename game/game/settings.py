97148f88e8ee0825f3b9d331e650e49ce6c883e2
Kiet Lam <kietdlam@gmail.com>
2015-03-27 05:17:07 -0700

    Added changes for deployment

--- game/game/settings.py
+++ game/game/settings.py
@@ -57,16 +57,6 @@ ROOT_URLCONF = 'game.urls'
 
 WSGI_APPLICATION = 'game.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -101,3 +91,8 @@ STATICFILES_DIRS = (
 
 
 APPEND_SLASH = True
+
+try:
+    from local_settings import *
+except:
+    pass
