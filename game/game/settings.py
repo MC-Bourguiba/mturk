f0a0f2739af05ce9da6c26a272ea590f49cb097a
Kiet Lam <kietdlam@gmail.com>
2015-03-27 05:52:39 -0700

    Updated settings.py

--- game/game/settings.py
+++ game/game/settings.py
@@ -130,11 +130,11 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(
-        os.path.dirname(__file__),
-        'static',
-    ),
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(
+    #     os.path.dirname(__file__),
+    #     'static',
+    # ),
+    # os.path.join(BASE_DIR, "static"),
 )
 
 
