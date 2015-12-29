35ffc195c4ed538bc79d7d79751d20b62e6fb918
Kiet Lam <kietdlam@gmail.com>
2015-12-29 10:53:27 -0800

    New TeX

--- paper/auto/paper.el
+++ paper/auto/paper.el
@@ -1,17 +1,48 @@
 (TeX-add-style-hook
  "paper"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithmic" "noend")))
    (TeX-run-style-hooks
     "latex2e"
+    "latex_commands"
     "sig-alternate-ipsn13"
-    "sig-alternate-ipsn1310")
+    "sig-alternate-ipsn1310"
+    "ntheorem"
+    "algorithm"
+    "algorithmic"
+    "todonotes"
+    "enumitem")
    (LaTeX-add-labels
-    "fig:cost_no_slider"
-    "fig:predicted_path_cost"
-    "fig:no_slider_player_flow_distribution"
-    "eq:KL_update"
-    "fig:kl_divergence_surface"
-    "fig:global_potential_function")
+    "sec:model"
+    "sec:routing_game"
+    "prop:potential"
+    "sec:learning"
+    "eq:MD_update"
+    "alg:MD"
+    "thm:convergence"
+    "sec:estimation"
+    "eq:MD_update_estimation"
+    "eq:estimation_eta"
+    "eq:hedge_solution"
+    "eq:estimation_alpha_eta0"
+    "subsec:reg_entropy"
+    "eq:propagation"
+    "fig:game_interface"
+    "sec:experiment"
+    "fig:system_block_diagram"
+    "fig:global_potential"
+    "sec:results"
+    "fig:estimated_distributions"
+    "fig:moving_average_learning_rate"
+    "fig:sequence_rate"
+    "fig:predictions_divergence"
+    "sec:conclusion")
+   (LaTeX-add-environments
+    "remark"
+    "theorem"
+    "definition"
+    "proposition")
    (LaTeX-add-bibliographies
-    "sigproc")))
+    "bib")))
 
