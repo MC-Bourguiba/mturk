ed22f79a3d20b885ea29f53525aa7538a698e455
MC-Bourguiba <med.chedly.bourguiba@gmail.com>
2016-05-13 01:28:51 +0200

    test

--- game/manage.py
+++ game/manage.py
@@ -4,9 +4,9 @@ import sys
 
 if __name__ == "__main__":
     sys.path.append('app/game/game')
+    print (sys.path)
     os.environ["DJANGO_SETTINGS_MODULE"]= "game.settings"
 
-    print sys.path
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
