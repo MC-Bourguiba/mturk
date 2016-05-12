d1365170c61d155dafc920812632ea9734bce35b
MC-Bourguiba <med.chedly.bourguiba@gmail.com>
2016-05-13 00:31:08 +0200

    test add app/game/game path

--- game/manage.py
+++ game/manage.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ["DJANGO_SETTINGS_MODULE"]= "game.settings"
     sys.path.append('app/game/game')
+    os.environ["DJANGO_SETTINGS_MODULE"]= "game.settings"
+
     print sys.path
     from django.core.management import execute_from_command_line
 
