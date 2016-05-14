d579c92c5ee9798683409448e90994777f2d4646
MC-Bourguiba <med.chedly.bourguiba@gmail.com>
2016-05-14 02:19:56 +0200

    postgres db

--- game/settings.py
+++ game/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
@@ -58,12 +60,13 @@ MIDDLEWARE_CLASSES = (
     # 'silk.middleware.SilkyMiddleware',
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
+#DATABASES = {
+    #'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+#  #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 ROOT_URLCONF = 'game.urls'
 
